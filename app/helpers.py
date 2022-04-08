2ef0cd7d0c365afc9f8ee355495b03a2b2bdbffe
XairOn1986 <k.sharabarov@gmail.com>
2022-04-08 13:18:23 -0400

    Login and Log Out

--- app/helpers.py
+++ app/helpers.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*- 
 import os
 import requests
 import urllib.parse
-from flask import render_template
+from flask import render_template, request, session, redirect
+from functools import wraps
 
 def apology(message, code=400):
     """Render message as an apology to user."""
@@ -16,3 +18,16 @@ def apology(message, code=400):
             s = s.replace(old, new)
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*- 
-from app.helpers import apology
-from flask import render_template, flash, redirect, request
+from app.helpers import apology, login_required
+from flask import render_template, flash, redirect, request, session
+from flask_session import Session
+from tempfile import mkdtemp
 from app import app
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,8 +11,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mysqldb import MySQL
 mysql = MySQL(app)
 
-#db.execute("SELECT username FROM users where username=%s", (username,))
-#message = db.fetchall()
+Session(app)
+sess = Session()
+sess.init_app(app)
 
 
 @app.route('/')
@@ -30,20 +33,20 @@ def register():
         db = mysql.connection.cursor()
         #Check username and password not empty
         if (len(username) == 0) or (len(password) == 0) or (len(email) == 0):
-            return apology("Input username, password, confirmation and EMAIL!")
+            return apology("Input name, password, confirmation and email!")
 
         #Check passwords correct repeats
         if password != confirmation:
-            return apology("Password confirmation missmatch!")
+            return apology("Repeat password correct!")
 
         #Check unic names and email
         unic_name = db.execute("SELECT username FROM users where username=%s", (username,))
         if unic_name != 0:
-            return apology("This name already in use!")
+            return apology("Username busy!")
         
         unic_name = db.execute("SELECT email FROM users where email=%s", (email,))
         if unic_name != 0:
-            return apology("This email already in use!")
+            return apology("Email busy!")
 
         hash = generate_password_hash(password)
         #Add the user's entry into the database after all checks
@@ -55,3 +58,48 @@ def register():
         return redirect("/") 
 
     return render_template("register.html")
+
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login(): 
+    """Log user in"""    
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        db = mysql.connection.cursor()
+        db.execute("SELECT * FROM users WHERE username = %s", (request.form.get("username"),))
+        rows = db.fetchall()
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["password_hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash('Welcome!')
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
