81c2a53136b87168a6e54753faf61c22692b03c2
XairOn1986 <k.sharabarov@gmail.com>
2022-04-08 03:25:39 -0400

    work with nginx

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*- 
+from flask import render_template
 from app import app
+import os
+import sqlite3
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, World!"
+def index(): 
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register(): 
+    return render_template("register.html")
